9bc8b33f6488a9aea7ea3d7b24373c13fa078e6b
lexi <128522383+LexiTheBestAlways@users.noreply.github.com>
2024-03-28 20:10:30 +0400

    post commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    standalone: true,
+    imports: [],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'aititi';
+  writeClassName(className: string): void {
+    document.write(className);
+  }
 }
